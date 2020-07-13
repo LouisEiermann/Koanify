5580afd6b3990ab8069fc31103b35660c80656ab
LouisEiermann <louis.eiermann@gmail.com>
2020-07-13 20:40:06 +0200

    added sound routing

--- app.js
+++ app.js
@@ -2,13 +2,25 @@
 const express = require("express");
 const app = express();
 
+// define routing
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + '/public/about.html')
+});
+
+app.get("/aboutKoans", function (req, res) {
+  res.sendFile(__dirname + '/public/aboutKoans.html')
+});
+
+app.get("/today", function (req, res) {
+  res.sendFile(__dirname + '/public/today.html')
+});
+
 // use the express-static middleware
 app.use(express.static("public"));
-
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
